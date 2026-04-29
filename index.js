e9ab798938e9c0bd97f6defe185e220852e85c4a
ayushkr504 <vasuuudev88@gmail.com>
2026-04-29 13:24:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('Bot online!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.login(process.env.DISCORD_TOKEN);
